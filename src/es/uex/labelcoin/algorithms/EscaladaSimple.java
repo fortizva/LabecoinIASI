6fe687e9bc2a6b7d40c41a406606db40acdf6010
MiguelAngel1602 <124580166+MiguelAngel1602@users.noreply.github.com>
2023-03-06 13:49:45 +0100

    EscaladaSimple

--- src/es/uex/labelcoin/algorithms/EscaladaSimple.java
+++ src/es/uex/labelcoin/algorithms/EscaladaSimple.java
@@ -1,13 +1,19 @@
 package es.uex.labelcoin.algorithms;
 
+import java.util.ArrayList;
+
 import es.uex.labelcoin.Coordenada;
 import es.uex.labelcoin.Tablero;
 import es.uex.labelcoin.util.Utils;
 
 public class EscaladaSimple {
 
+	public static ArrayList<Tablero.Movimiento> camino = new ArrayList<>();
+
 	public static void main(String[] args) {
-		// TODO Implementar Escalada simple
+		// TODO Implementar Escaladad Simple
+		System.out.println("Labelcoin: Escalada Simple");
+		System.out.println(Utils.getDistancia(new Coordenada(4, 1), new Coordenada(3, 1)));
 		// Obtener fichero de argumento (si es que hay)
 		String path = (args.length == 1) ? args[0] : "res/LABECOIN1.txt";
 		// Carga del tablero
@@ -18,14 +24,74 @@ public class EscaladaSimple {
 		} else
 			System.exit(1);
 
-		/*
-		 * Demostración de la utilidad para medir distancia entre dos puntos del tablero
-		 * Lo usaremos para la heurística
-		 */
-		Coordenada a = new Coordenada(1, 2);
-		Coordenada b = new Coordenada(3, 1);
-		System.out.println("DEBUG: Distancia entre dos puntos " + a.toString() + " y " + b.toString() + ": "
-				+ Utils.getDistancia(a, b));
+		boolean success = escaladaSimple(t);
+		System.out.println();
+		Utils.printCamino(camino, success);
 	}
 
+	public static boolean escaladaSimple(Tablero t) {
+		boolean success = false, end = false, found=false;
+		Tablero.Movimiento mov = Tablero.Movimiento.Abajo;
+		Coordenada objetivo = new Coordenada(-1, -1);
+        int i=0;
+       Tablero.Movimiento currentMov;
+		// Seleccionamos el primer objetivo
+		objetivo = seleccionarObjetivo(t);
+		System.out.println("DEBUG: Primer objetivo = " + objetivo);
+		// Obtenemos las h' disponibles y escogemos la mejor
+		Coordenada c, tmp;
+		while (!end) {
+			end = true;
+			c = t.robot;
+			found=false;
+			i=0;
+			while(!found && i<Tablero.Movimiento.values().length) {
+				currentMov=Tablero.Movimiento.values()[i];
+				tmp = Utils.calcularCoordenada(t.robot, currentMov);
+				if (Utils.getDistancia(tmp, objetivo) < Utils.getDistancia(c, objetivo) && t.comprobarMovimiento(currentMov)) {
+					c = tmp;
+					mov = currentMov;
+					end = false;
+					found=true;
+				}
+				i++;
+			}
+			
+			if (!end) {
+				// Mover robot
+				t.moverRobot(mov);
+				camino.add(mov);
+				if (t.robot.equals(objetivo)) {
+					if (t.monedas.containsKey(objetivo)) {
+						t.monedas.remove(objetivo);
+						objetivo = seleccionarObjetivo(t);
+						System.out.println("DEBUG: Nuevo objetivo = "+objetivo);
+					} else // Caso de ser la salida
+						end = true;
+				}
+			}
+		}
+		return success;
+	}
+
+	public static Coordenada seleccionarObjetivo(Tablero t) {
+		Coordenada objetivo = null;
+		double dist = -1; // Empleamos la distancia al objetivo como h'
+		if (t.getPrecio() > t.getCartera()) {
+			double tmp;
+			for (int i = 0; i < t.monedas.size(); i++) {
+				tmp = Utils.getDistancia(t.robot, (Coordenada) t.monedas.keySet().toArray()[i]);
+				// TODO: Elegir la moneda de más valor a misma distancia.
+				if (dist == -1 || tmp <= dist) {
+					dist = tmp;
+					objetivo = (Coordenada) t.monedas.keySet().toArray()[i];
+				}
+			}
+		} else {
+			objetivo = t.salida;
+			dist = Utils.getDistancia(t.robot, t.salida);
+		}
+
+		return objetivo;
+	}
 }
