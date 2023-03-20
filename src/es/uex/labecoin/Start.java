246369a1d6eb38a395e0674cff2637563293d59d
fortizva <fortizva@alumnos.unex.es>
2023-03-20 14:21:40 +0100

    Implementado EscaladaSimpleEstocastico

--- src/es/uex/labecoin/Start.java
+++ src/es/uex/labecoin/Start.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.Scanner;
 
 import es.uex.labecoin.algorithms.EscaladaSimple;
+import es.uex.labecoin.algorithms.EscaladaSimpleEstocastico;
 import es.uex.labecoin.algorithms.MaximaPendiente;
 
 public class Start {
@@ -33,7 +34,16 @@ public class Start {
 			while (!valid) {
 				System.out.println("\t1)\tEscalada simple");
 				System.out.println("\t2)\tMaxima pendiente");
-				int selection = sc.nextInt();
+				System.out.println("\t3)\tEscalada simple estocástico");
+				int selection;
+				try {
+					selection = Integer.parseInt(sc.nextLine());
+				} catch (NumberFormatException e) {
+					// Si hay un error al leer la opción introducida establecemos selection a -1
+					// para que salte el error
+					selection = -1;
+				}
+
 				switch (selection) {
 				case 1:
 					valid = true;
@@ -42,6 +52,27 @@ public class Start {
 				case 2:
 					valid = true;
 					MaximaPendiente.launch(t);
+					break;
+				case 3:
+					valid = true;
+					boolean validMejora = false;
+					int mejora = 1;
+					while (!validMejora) {
+						System.out.println("Cantidad de mejora:");
+						try {
+							mejora = Integer.parseInt(sc.nextLine());
+
+							if (mejora >= 0)
+								validMejora = true;
+							else
+								throw new Exception();
+						} catch (Exception e) {
+							System.out.println(
+									"Seleccion invalida! La cantidad de mejora debe ser un número entero positivo:");
+						}
+					}
+
+					EscaladaSimpleEstocastico.launch(t, mejora);
 					break;
 				default:
 					System.out.println("Seleccion invalida! Pruebe con una de las siguientes opciones:");
---
+++ src/es/uex/labecoin/algorithms/EscaladaSimpleEstocastico.java
@@ -0,0 +1,91 @@
+package es.uex.labecoin.algorithms;
+
+import java.util.ArrayList;
+
+import es.uex.labecoin.Coordenada;
+import es.uex.labecoin.Tablero;
+import es.uex.labecoin.util.Utils;
+
+public class EscaladaSimpleEstocastico {
+
+	public static ArrayList<Tablero.Movimiento> camino = new ArrayList<>();
+
+	public static void launch(Tablero t, int mejora) {
+		System.out.println("Labecoin: Escalada simple estocastico");
+
+		boolean success = escaladaSimpleEstocastico(t, mejora);
+		System.out.println();
+		Utils.printCamino(camino, success);
+	}
+
+	public static boolean escaladaSimpleEstocastico(Tablero t, int mejora) {
+		boolean success = false, end = false, found = false;
+		Tablero.Movimiento mov = Tablero.Movimiento.Abajo;
+		Coordenada objetivo = new Coordenada(-1, -1);
+		int i = 0;
+		Tablero.Movimiento currentMov;
+		// Seleccionamos el primer objetivo
+		objetivo = seleccionarObjetivo(t);
+		System.out.println("DEBUG: Primer objetivo = " + objetivo);
+		// Obtenemos las h' disponibles y escogemos la mejor
+		Coordenada c, tmp;
+		while (!end) {
+			end = true;
+			c = t.robot;
+			found = false;
+			i = 0;
+			while (!found && i < Tablero.Movimiento.values().length) {
+				currentMov = Tablero.Movimiento.values()[i];
+				tmp = Utils.calcularCoordenada(t.robot, currentMov);
+				if (Utils.getDistancia(tmp, objetivo) < Utils.getDistancia(c, objetivo)
+						&& (Utils.getDistancia(c, objetivo) - Utils.getDistancia(tmp, objetivo) >= mejora)
+						&& t.comprobarMovimiento(currentMov)) {
+					c = tmp;
+					mov = currentMov;
+					end = false;
+					found = true;
+				}
+				i++;
+			}
+
+			if (!end) {
+				// Mover robot
+				t.moverRobot(mov);
+				camino.add(mov);
+				if (t.robot.equals(objetivo)) {
+					if (t.monedas.containsKey(objetivo)) {
+						t.monedas.remove(objetivo);
+						objetivo = seleccionarObjetivo(t);
+						System.out.println("DEBUG: Nuevo objetivo = " + objetivo);
+					} else {// Caso de ser la salida
+						end = true;
+						if (t.price <= t.wallet)
+							success = true;
+					}
+				}
+			}
+		}
+		return success;
+	}
+
+	public static Coordenada seleccionarObjetivo(Tablero t) {
+		Coordenada objetivo = null;
+		double dist = -1; // Empleamos la distancia al objetivo como h'
+		if (t.getPrecio() > t.getCartera()) {
+			double tmp;
+			for (int i = 0; i < t.monedas.size(); i++) {
+				tmp = Utils.getDistancia(t.robot, (Coordenada) t.monedas.keySet().toArray()[i]);
+				// TODO: Elegir la moneda de más valor a misma distancia.
+				if (dist == -1 || tmp <= dist) {
+					dist = tmp;
+					objetivo = (Coordenada) t.monedas.keySet().toArray()[i];
+				}
+			}
+		} else {
+			objetivo = t.salida;
+			dist = Utils.getDistancia(t.robot, t.salida);
+		}
+
+		return objetivo;
+	}
+}
